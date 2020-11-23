07cdb6a4dde7daf9d9bcf0eeff7d6ff46a97c08c
Eddy Chase <eddykanyz.ek@gmail.com>
2020-11-23 21:39:28 +0300

    Addition of setUp method

--- credentials.test.py
+++ credentials.test.py
@@ -2,4 +2,7 @@ import unittest
 from credentials import Credentials
 
 class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        self.new_credentials=Credentials("Eddy","Chase","Goog97")
 
+    
