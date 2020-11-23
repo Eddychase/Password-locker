98556451cab4b1f4d6a5ddcd6262e17296eda74a
Eddy Chase <eddykanyz.ek@gmail.com>
2020-11-23 20:51:14 +0300

    Addition of method for saving credentials

--- credentials.py
+++ credentials.py
@@ -1,7 +1,11 @@
 class Credentials:
-    credential_list=[]
+    credentials_list=[]
 
     def __init__(self,first_name,last_name,password):
         self.first_name=first_name
         self.last_name=last_name
         self.password=password
+
+    def save_credentials(self):
+        Credentials.credentials_list.append(self)
+
