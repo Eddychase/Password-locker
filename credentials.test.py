ba69ab724fe6f3955d060a739e37c0493a55281b
Eddy Chase <eddykanyz.ek@gmail.com>
2020-11-23 21:40:44 +0300

    Addition of method to check if object was initialized properly

--- credentials.test.py
+++ credentials.test.py
@@ -5,4 +5,10 @@ class TestCredentials(unittest.TestCase):
     def setUp(self):
         self.new_credentials=Credentials("Eddy","Chase","Goog97")
 
+    def test__init(self):
+        self.assertEqual(self.new_credentials.first_name,"Eddy")
+        self.assertEqual(self.new_credentials.last_name,"Chase")
+        self.assertEqual(self.new_credentials.password,"Goog97")
+
+
     
