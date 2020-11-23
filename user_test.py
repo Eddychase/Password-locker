6e5ef5878474537e217201143b9d22ee85501230
Eddy Chase <eddykanyz.ek@gmail.com>
2020-11-23 23:35:27 +0300

    Addition of constructor test

--- user_test.py
+++ user_test.py
@@ -5,6 +5,17 @@ from credentials import Credentials
 class TestUser(unittest.TestCase):
 
     def setUp(self):
-         self.new_user=User("Eddy","Chase")
+         self.new_user=User("Eddy","Chase97")
+
+    def tearDown(self):
+        User.user_list=[]
+
+    def test__init(self):
+        self.assertEqual(self.new_user.user_name,"Eddy")
+        self.assertEqual(self.new_user.user_password,"Chase97")
+
+if __name__=='__main__':
+    unittest.main()
+
 
 
