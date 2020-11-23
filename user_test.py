082a60cfa2174c30c72a6e001f942194876088db
Eddy Chase <eddykanyz.ek@gmail.com>
2020-11-23 23:25:20 +0300

    Addition of user_test class

---
+++ user_test.py
@@ -0,0 +1,10 @@
+import unittest
+from user import User
+from credentials import Credentials
+
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+         self.new_user=User("Eddy","Chase","Goog97")
+
+
