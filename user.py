fcdf83d062848a74857bfd8bcc4a5c2c8a8c65ee
Eddy Chase <eddykanyz.ek@gmail.com>
2020-11-23 22:30:34 +0300

    Addition of class constructor

--- user.py
+++ user.py
@@ -1,3 +1,7 @@
 from credentials import Credentials
 class User:
     user_list=[]
+
+    def __init__(self,user_name,user_password):
+        self.user_name=user_name
+        self.user_password=user_password
